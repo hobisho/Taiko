0ee8891e1b0fae9630f2ef9b5985dd956b2c568b
hobisho <hobisho0512@gmail.com>
2025-03-03 23:55:18 +0800

    {fix}fix 16 numbers can't not print 1&2 error

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import re
 
-extracted_numbers = []
+extracted_numbers = ["3000000011111010"]
 word= ""
             #if it is 16 numbers
 if (extracted_numbers==[]):
@@ -10,7 +10,6 @@ if (extracted_numbers==[]):
 elif ((len(extracted_numbers[0])%4)==0) :
     #small than 16
     if(len(extracted_numbers[0])<16):
-        print("small than 16")
         multiply = int(16/len(extracted_numbers[0]))
         for d in extracted_numbers[0]:
             if ((d=="1")|(d=="2")):
@@ -25,20 +24,22 @@ elif ((len(extracted_numbers[0])%4)==0) :
 
     #greater than 16
     else:
-        print("greater than 16")
         divide = int(len(extracted_numbers[0])/16)
         for k in range(0,len(extracted_numbers[0]),divide):
-            d = str(max(int(extracted_numbers[0][k]),int(extracted_numbers[0][k+1]),int(extracted_numbers[0][k+2])))
-            if (d=="3"):
+            d = str(max(int(extracted_numbers[0][i])for i in range(k,k+divide)))
+            if ((d=="1")|(d=="2")):
+                d=d
+            elif (d=="3"):
                 d="1"
             elif (d=="4"):
                 d="2"
             else:
                 d="0"
             word =  ''.join(word + d)
-
-    extracted_numbers = word
+    word_list = [word]
 
 #if it can't be devide by 4
 else:
     print("error")
+
+print(word_list)
--- v2/tjaread/tjaread.py
+++ v2/tjaread/tjaread.py
@@ -24,13 +24,15 @@ def parse_tja_file(file_path):
             
             #if it is 16 numbers
             if (extracted_numbers==[]):
-                extracted_numbers = ["0"*16]
+                word_list = ["0"*16]
+            
+            # elif (len(extracted_numbers[0])==16):
+            #     word_list = extracted_numbers
 
             #if it can be devide by 4 and not 16 numbers
             elif ((len(extracted_numbers[0])%4)==0) :
                 #small than 16
                 if(len(extracted_numbers[0])<16):
-                    print("small than 16")
                     multiply = int(16/len(extracted_numbers[0]))
                     for d in extracted_numbers[0]:
                         if ((d=="1")|(d=="2")):
@@ -48,26 +50,26 @@ def parse_tja_file(file_path):
                     divide = int(len(extracted_numbers[0])/16)
                     for k in range(0,len(extracted_numbers[0]),divide):
                         d = str(max(int(extracted_numbers[0][i])for i in range(k,k+divide)))
-                        if (d=="3"):
+                        if ((d=="1")|(d=="2")):
+                            d=d
+                        elif (d=="3"):
                             d="1"
                         elif (d=="4"):
                             d="2"
                         else:
                             d="0"
                         word =  ''.join(word + d)
-
-                extracted_numbers = word
+                word_list = [word]
 
             #if it can't be devide by 4
             else:
                 print("error")
                 
-            numbers.extend(extracted_numbers)
-    
-    return numbers
+            numbers.extend(word_list)
+    return numbers,extracted_numbers
 
 # 測試用
 if __name__ == "__main__":
     file_path = "level 6~7/02. TT -Japanese ver.-/TT -Japanese ver.-.tja"  # 這裡請換成你的.tja檔案路徑
-    result = parse_tja_file(file_path)
-    # print(result)
+    result,output_numbers = parse_tja_file(file_path)
+    print(result)
