7e8a5c624500e91129afd8b29fba35daf8539e1a
hobisho <hobisho0512@gmail.com>
2025-03-04 23:49:11 +0800

    {add} add a compression_file funtion

--- v2/compression/decompression.py
+++ v2/compression/decompression.py
@@ -2,38 +2,48 @@ import tensorflow as tf
 import numpy as np
 
 # TFRecord 檔名
-tfrecords_filename = 'taiko.tfrecords'
+def decompression(tfrecords_filename):
+    tfrecords_filename = tfrecords_filename
 
-# 建立 TFRecordDataset
-dataset = tf.data.TFRecordDataset(tfrecords_filename)
+    # 建立 TFRecordDataset
+    dataset = tf.data.TFRecordDataset(tfrecords_filename)
 
-# 定義 TFRecord 格式
-feature_description = {
-    'height': tf.io.FixedLenFeature([], tf.int64),
-    'width': tf.io.FixedLenFeature([], tf.int64),
-    'depth': tf.io.FixedLenFeature([], tf.int64),
-    'image_string': tf.io.FixedLenFeature([], tf.string),
-    'label': tf.io.FixedLenFeature([], tf.float32),
-}
+    # 定義 TFRecord 格式
+    feature_description = {
+        'height': tf.io.FixedLenFeature([], tf.int64),
+        'width': tf.io.FixedLenFeature([], tf.int64),
+        'depth': tf.io.FixedLenFeature([], tf.int64),
+        'image_string': tf.io.FixedLenFeature([], tf.string),
+        'label': tf.io.FixedLenFeature([], tf.float32),
+    }
 
-# 解析函式
-def _parse_function(proto):
-    return tf.io.parse_single_example(proto, feature_description)
+    # 解析函式
+    def _parse_function(proto):
+        return tf.io.parse_single_example(proto, feature_description)
 
-# 解析 TFRecord
-parsed_dataset = dataset.map(_parse_function)
+    # 解析 TFRecord
+    parsed_dataset = dataset.map(_parse_function)
 
-# 逐筆讀取 TFRecord
-for record in parsed_dataset:
-    height = int(record['height'].numpy())
-    width = int(record['width'].numpy())
-    depth = int(record['depth'].numpy())
-    image_string = record['image_string'].numpy()
-    label = float(record['label'].numpy())
+    # 逐筆讀取 TFRecord
+    image_list =[]
+    label_list = []
+    for record in parsed_dataset:
+        height = int(record['height'].numpy())
+        width = int(record['width'].numpy())
+        depth = int(record['depth'].numpy())
+        image_string = record['image_string'].numpy()
+        label = float(record['label'].numpy())
 
-    # 將 bytes 轉換回 NumPy 陣列
-    image_1d = np.frombuffer(image_string, dtype=np.uint8)
-    image = image_1d.reshape((height, width, depth))
+        # 將 bytes 轉換回 NumPy 陣列
+        image_1d = np.frombuffer(image_string, dtype=np.uint8)
+        image = image_1d.reshape((height, width, depth))
+        image_list.append(image)
+        label_list.append(label)
 
-    print(f"讀取圖片: {height}x{width}，標籤: {label}，data{image}")
+    return image_list, label_list 
+    
+if __name__ == '__main__':
+    tfrecords_filename = 'taiko.tfrecords'
+    image_list, label_list = decompression(tfrecords_filename)
+    print(image_list[0])
 
