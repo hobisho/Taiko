6ce286e533a6065a56496b7845db3af98548fdf3
hobisho <hobisho0512@gmail.com>
2025-03-06 01:20:46 +0800

    {start}start test main.py

---
+++ v2/label/bk.py
@@ -0,0 +1,11 @@
+def break_str(string_list):
+    out=[]
+    for string in string_list:
+        for count in range (16):
+            out.append(string[count])
+    print(out)
+    return out
+
+if __name__ == '__main__':
+    a=['2200000000220022','0000000000000000']
+    break_str(a)
